d521775e73fa573e5b6c45bd3627909b7da3593d
LnwThaiz <tonnamnattapat@gmail.com>
2023-08-09 16:28:21 +0700

    LeaveStatus

--- LeaveHistory.php
+++ LeaveHistory.php
@@ -1,52 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <style>
+    .table-container {
+        padding-top: 10px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto
+    }
 
-.table-container {
-    padding-top : 10px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto
-}
+    h3 {
+        padding-top: 50px;
+        margin: 73px;
+    }
 
-h3 {
-    padding-top : 50px;
-    margin: 73px;
-}
+    .table {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        width: 90%;
+    }
 
-.table {
-    border-collapse : collapse;
-    margin: 25px 0;
-    font-size: 0.9em;
-    width: 90%;
-}
+    .table thead tr th {
+        background-color: #454ABB;
+        color: #ffffff;
+        border: 1px solid white;
+        height: 50px;
+        font-weight: bold;
+    }
 
-.table thead tr th{
-    background-color: #454ABB;
-    color: #ffffff;
-    border: 1px solid white;
-    height: 50px;
-    font-weight: bold;
-}
-
-.table tbody tr td{
-    background-color: white;
-    border: 1px solid white;
-}
-
-
-
+    .table tbody tr td {
+        background-color: white;
+        border: 1px solid white;
+    }
 </style>
 
 <body>
 
-    <?php 
-        include "navbar.php"
+    <?php
+    include "navbar.php"
     ?>
 
     <H3 class="align-left text-left mb-0">ประวัติการลา</H3>
@@ -67,39 +64,66 @@ h3 {
                     <td>ลาป่วย</td>
                     <td>25/05/66 09:00 - 26/05/2566 18:00</td>
                     <td>รออนุมัติ</td>
-                    <td><a href="" class="btn p-0 border-0 shadow-none"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
+                    <td><a href="" class="btn p-0 border-0 shadow-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
                 </tr>
                 <tr>
                     <td>23/05/2566</td>
                     <td>ลาป่วย</td>
                     <td>25/05/66 09:00 - 26/05/2566 18:00</td>
                     <td>รออนุมัติ</td>
-                    <td><a href="" class="btn p-0 border-0 shadow-none"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
+                    <td><a href="" class="btn p-0 border-0 shadow-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
                 </tr>
                 <tr>
                     <td>23/05/2566</td>
                     <td>ลาป่วย</td>
                     <td>25/05/66 09:00 - 26/05/2566 18:00</td>
                     <td>รออนุมัติ</td>
-                    <td><a href="" class="btn p-0 border-0 shadow-none"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
+                    <td><a href="" class="btn p-0 border-0 shadow-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
                 </tr>
                 <tr>
                     <td>23/05/2566</td>
                     <td>ลาป่วย</td>
                     <td>25/05/66 09:00 - 26/05/2566 18:00</td>
                     <td>รออนุมัติ</td>
-                    <td><a href="" class="btn p-0 border-0 shadow-none"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
+                    <td><a href="" class="btn p-0 border-0 shadow-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
                 </tr>
                 <tr>
                     <td>23/05/2566</td>
                     <td>ลาป่วย</td>
                     <td>25/05/66 09:00 - 26/05/2566 18:00</td>
                     <td>รออนุมัติ</td>
-                    <td><a href="" class="btn p-0 border-0 shadow-none"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
+                    <td><a href="" class="btn p-0 border-0 shadow-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-info-circle-fill fs-5"></i></a></td>
                 </tr>
             </tbody>
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-fullscreen py-4 px-2">
+                <div class="modal-content">
+                    <div class="modal-header d-flex flex-column border-0" >
+                        <div class="d-flex justify-content-between w-100 px-3 align-items-center">
+                            <i class="bi bi-file-text-fill fs-1"></i>
+                            <h1 class="modal-title fs-4" id="staticBackdropLabel">รายละเอียดการลา</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="w-100">
+                            <hr class="border border-3 border-dark mx-3 opacity-100">
+                        </div>
+                    
+                    </div>
+                    <div class="modal-body mx-3">
+                        yed mae
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  
+
 </body>
+
 </html>
