d195f861147527386335fe2d552d6c814bb20f16
Jeng GG <65309010007@tatc.ac.th>
2023-08-13 00:31:48 +0700

    Update Sidebar1

--- Test.php
+++ Test.php
@@ -1,40 +1,123 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<link rel="stylesheet" href="Test.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="Test5.css">
 </head>
+
 <body>
+  <?php include_once "Testnavbar.php";
+        include_once "Testsider.php";
+  ?>
 
-<!-- ปุ่มแรกเปิดปฏิทิน -->
-<button id="openPopupButton1">เปิดปฏิทิน 1</button>
+  <div  style="width: 100%; height: 100%; position: relative; background: white">
+    <div class="form-box" style="left: 442px; top: 100px;"></div>
+    <div class="textlabel" style="width: 136px; height: 30px; left: 486px; top: 260px;">เหตุผลการลา</div>
 
-<!-- ปุ่มที่สองเปิดปฏิทิน -->
-<button id="openPopupButton2">เปิดปฏิทิน 2</button>
+    <!-- กล่องคอมเมนต์ -->
+    <textarea style="left: 479px; top: 290px; resize: none; border-radius: 5px;
+        font-family : 'Anakotmai',sans-serif ;font-size: medium"></textarea>
 
-<!-- ป็อปอัพท์ปฏิทิน 1 -->
-<div class="calendar-popup" id="calendarPopup1">
-  <h2 id="currentMonthYear1">ปฏิทิน 1</h2>
-  <div class="calendar-header">
-    <button id="prevMonthButton1">ย้อนกลับ</button>
-    <span id="currentMonthYear1">เดือน ปี</span>
-    <button id="nextMonthButton1">ถัดไป</button>
+    <!-- ข้อมูลฝั่งขวาของกล่องข้อความการลา -->
+    <div class="divider2" style="left: 700px; top: 196px;"></div>
+    <div class="divider2" style="left: 1126px; top: 195px;"></div>
+    <div style="width: 165px; height: 75px; left: 818px; top: 50px; position: absolute; color: black;
+        font-size: 30px; font-family: 'Anakotmai',sans-serif; font-weight: 400; word-wrap: break-word">
+      บันทึกการลา</div>
+    <div class="file-input" style="left: 486px; top: 490px; width: auto;">
+      <p style="font-size: 15px; color: red; margin-bottom: 0;">** หมายเหตุ กรณีที่ลาป่วยเกิน 3 วันควรมีใบรับรองแพทย์ในการยืนยัน</p>
+      <p class="mb-0">แนบไฟล์ ใบรับรองแพทย์</p>
+    </div>
+    <div class="file" style="left: 486px; top: 545px;"><input type="file"></div>
+
+    <!-- ปุ่มการเลือกข้อมูล -->
+    <div class="file-input" style="left: 1044px; top: 260px;">ประเภทการลา</div>
+    <div>
+      <select style="width: 159px; height: 50px; left: 1050px; top: 310px; position: absolute; background: white;
+            border-radius: 15px; font-size: 20px; font-family: 'Anakotmai',sans-serif; font-weight: 50;
+            word-wrap: break-word" name="Type_of_eave">
+        <option>ลากิจ</option>
+        <option>ลาป่วย</option>
+      </select>
+    </div>
+    <div class="file-input" style="left: 1044px; top: 371px;">วิชาที่ลาได้</div>
+    <div>
+      <select style="width: 159px; height: 50px; left: 1050px; top: 424px; position: absolute; background: white;
+            border-radius: 15px;  font-size: 20px; font-family: 'Anakotmai',sans-serif; font-weight: 50;
+            word-wrap: break-word" name="Leaving_subjects">
+        <option>คณิตศาสตร์</option>
+        <option>วิทยาศาสตร์</option>
+      </select>
+    </div>
+
+    <!-- ส่วนของเวลา -->
+    <div class="file-input" style="left: 964px; top: 130px;">ตั้งแต่เวลา</div>
+    <div>
+      <select style="width: 159px; height: 72px; left: 956px; top: 159px; position: absolute; background: white;
+            border-radius: 15px; color: black; font-size: 20px; font-family: 'Anakotmai',sans-serif; font-weight: 400;
+            word-wrap: break-word" name="Type_of_Leave">
+        <option>10.00 - 11.00</option>
+        <option>11.00 - 12.00</option>
+      </select>
+    </div>
+
+    <div class="file-input" style="left: 1180px; top: 130px;">ถึงเวลา</div>
+    <div>
+      <select name="Type_of_eave" style="width: 159px; height: 72px; left: 1169px; top: 159px; position: absolute;
+            background: white; border-radius: 15px; color: black; font-size: 20px; font-family: 'Anakotmai',sans-serif;
+            font-weight: 400; word-wrap: break-word">
+        <option>11.00 - 12.00</option>
+        <option>12.00 - 13.00</option>
+      </select>
+    </div>
+    <div class="file-input" style="left: 486px; top: 130px;">วันที่เริ่มลา</div>
+    <div>
+      <!-- ปุ่มที่สองเปิดปฏิทิน -->
+      <button id="openPopupButton2" style="left: 720px; top: 159px;">เลือกวันที่</button>
+    </div>
+    <div class="file-input" style="left: 725px; top: 130px;">ถึงวันที่</div>
+    <div>
+
+      <!-- ปุ่มแรกเปิดปฏิทิน -->
+      <button id="openPopupButton1" style="left: 479px; top: 159px;">เลือกวันที่</button>
+
+    </div>
+    <div>
+      <button class="submit-button" style="left: 1050px; top: 530px;" onclick="window.location.href = 'index.php';">ยืนยันการลา</button>
+    </div>
+
+
+
+    <!-- ป็อปอัพท์ปฏิทิน 1 -->
+    <div class="calendar-popup" style="left:479px; top: 235px;" id="calendarPopup1">
+      <h2 id="currentMonthYear1">ปฏิทิน 1</h2>
+      <div class="calendar-header">
+        <button id="prevMonthButton1">ย้อนกลับ</button>
+        <span id="currentMonthYear1">เดือน ปี</span>
+        <button id="nextMonthButton1">ถัดไป</button>
+      </div>
+      <div class="calendar" id="calendar1">
+      </div>
+    </div>
+
+    <!-- ป็อปอัพท์ปฏิทิน 2 -->
+    <div class="calendar-popup" style="left: 720px; top: 235px;" id="calendarPopup2">
+      <h2 id="currentMonthYear2">ปฏิทิน 2</h2>
+      <div class="calendar-header">
+        <button id="prevMonthButton2">ย้อนกลับ</button>
+        <span id="currentMonthYear2">เดือน ปี</span>
+        <button id="nextMonthButton2">ถัดไป</button>
+      </div>
+      <div class="calendar" id="calendar2">
+      </div>
+    </div>
+
+
+    <script src="popUpTest.js"></script>
   </div>
-  <div class="calendar" id="calendar1">
-  </div>
-</div>
-
-<!-- ป็อปอัพท์ปฏิทิน 2 -->
-<div class="calendar-popup" id="calendarPopup2">
-  <h2 id="currentMonthYear2">ปฏิทิน 2</h2>
-  <div class="calendar-header">
-    <button id="prevMonthButton2">ย้อนกลับ</button>
-    <span id="currentMonthYear2">เดือน ปี</span>
-    <button id="nextMonthButton2">ถัดไป</button>
-  </div>
-  <div class="calendar" id="calendar2">
-  </div>
-</div>
-
-<script src ="popUpd.js"></script>
 </body>
+
 </html>
---
+++ Testnavbar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Leave System</title>
+</head>
+
+<body>
+    <?php
+    include_once "BSCss.html";
+
+    session_start();
+
+    // ตรวจสอบว่ามีตัวแปรเซสชัน username และ fullname ถูกตั้งค่าหรือไม่
+    if (isset($_SESSION["STD_ID"])) {
+        $STD_ID = $_SESSION["STD_ID"];
+        $STD_Name = $_SESSION["STD_Name"];
+        $STD_Lastname = $_SESSION["STD_Lastname"];
+        $STD_Birth = $_SESSION["STD_Birth"];
+        $STD_Phone = $_SESSION["STD_Phone"];
+        $Classlev_ID = $_SESSION["Classlev_ID"];
+        $Major_ID = $_SESSION["Major_ID"];
+        $Parent_Name = $_SESSION["Parent_Name"];
+        $STD_Address = $_SESSION["STD_Address"];
+        $Group_ID = $_SESSION["Group_ID"];
+        $Provinces_ID = $_SESSION["provinces_id"];
+        $District_ID = $_SESSION["district_id"];
+        $SubDistrict_ID = $_SESSION["subdistrict_id"];
+    } else {
+        // ถ้าไม่มีตัวแปรเซสชัน username แสดงว่ายังไม่ได้ล็อกอิน
+        echo "กรุณาล็อกอินก่อนใช้งาน!";
+    }
+    ?>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background: #454ABB;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-2">
+                <li>
+                    <button id="sidebarToggle">☰</button>
+
+                </li>
+                <li class="nav-item ml-1">
+                    <img src="./img/TATC_Logo.png" width="70" class="rounded-circle border-10px">
+                </li>
+
+                <li class="nav-item mt-2 ml">
+                    <a class="nav-link text-white" href="index.php">วิทยาลัยเทคนิคสัตหีบ</a>
+                </li>
+
+
+
+            </ul>
+
+        </div>
+    </nav>
+    <?php
+    include_once "footerBoostrapLogin.html";
+    ?>
+</body>
+
+</html>
