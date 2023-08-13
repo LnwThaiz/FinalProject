0b0d0582cd0e9870ff6c50ed775b9706eed0e849
LnwThaiz <tonnamnattapat@gmail.com>
2023-08-14 02:37:23 +0700

    update leavehis

--- LeaveHistory.php
+++ LeaveHistory.php
@@ -158,7 +158,7 @@
                                     <div class=" col">
                                         <div class=" d-flex align-items-center m-0">
                                             <p class=" text-nowrap m-0">เหตุผลการลา :</p>
-                                            <textarea name="" id="" class=" form-control "></textarea>
+                                            <input type="text" class=" form-control shadow-none disabled" disabled>
                                         </div>
                                     </div>
                                 </div>
@@ -192,12 +192,112 @@
                     <div class="modal-footer d-flex justify-content-between border-0">
                         <button type="button" class="btn btn-dark">พิมพ์เอกสาร</button>
                         <div>
-                            <button type="button" class="btn btn-primary">แก้ไขข้อมูล</button>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">แก้ไขข้อมูล</button>
                             <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ยกเลิกการลา</button>
                         </div>
                     </div>
                 </div>
-            </div>
+
+                <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-fullscreen py-4 px-5">
+                        <div class="modal-content">
+                            <div class="modal-header d-flex flex-column border-0">
+                                <div class="d-flex justify-content-between w-100 px-3 align-items-center">
+                                    <i class="bi bi-file-text-fill fs-1"></i>
+                                    <h1 class="modal-title fs-4" id="staticBackdropLabel">รายละเอียดการลา</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="w-100">
+                                    <hr class="border border-3 border-dark mx-3 opacity-100">
+                                </div>
+                            </div>
+                            <div class="modal-body">
+                                <div class="container d-flex flex-column">
+                                    <div class="row row-cols-1 " style="border: 1px solid red;align-items: center;">
+                                        <div class=" row row-cols-3 g-3">
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">วันที่เริ่มลา :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">เวลา :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">ประเภทการลา :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">ถึงวันที่ :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">เวลา :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">วิชาที่ลาได้ :</p>
+                                                    <select name="" id="" class=" form-control">
+                                                        <option value="">1</option>
+                                                        <option value="">2</option>
+                                                        <option value="">3</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class=" col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <p class=" text-nowrap m-0">เหตุผลการลา :</p>
+                                                    <input type="text" class=" form-control shadow-none disabled" disabled>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="container d-flex flex-column mt-5">
+                                    <div class="row row-cols-1" style="border: 1px solid red;">
+                                        <div class="row row-cols-2 d-flex justify-content-between">
+                                            <div class="col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <label class=" text-nowrap m-0">วันที่ทำการลา :</label>
+                                                    <label class="mx-1"> xx/xx/xxxx</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="container d-flex flex-column mt-1">
+                                    <div class="row row-cols-1" style="border: 1px solid red;">
+                                        <div class="row row-cols-2 d-flex justify-content-between">
+                                            <div class="col">
+                                                <div class=" d-flex align-items-center m-0">
+                                                    <label class=" text-nowrap m-0">สถานะการดำเนินการ :</label>
+                                                    <label class="mx-1"> รออนุมัติ</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer d-flex justify-content-between border-0">
+                                <button type="button" class="btn btn-dark">พิมพ์เอกสาร</button>
+                                <div>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">แก้ไขข้อมูล</button>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ยกเลิกการลา</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
 </body>
 
