56cc2e6e830cc8c3d3308f5d51105b6915d2ee6f
LnwThaiz <tonnamnattapat@gmail.com>
2023-09-27 02:16:12 +0700

    PDF

---
+++ PDFleave.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+</head>
+<style>
+    ::-webkit-scrollbar {
+        height: 0;
+        width: 0;
+    }
+</style>
+
+<body>
+    <div class="cotent" style="font-family: 'Sarabun';">
+        <div class="container border border-1 mt-3" style="width:55rem;">
+            <div>
+                <div class="d-flex justify-content-end mt-3">
+                    <div class="align-items-end">
+                        <div>
+                            <label for="">วิทยาลัยเทคนิคสัตหีบ ต.นาจอมเทียน</label>
+                        </div>
+                        <div>
+                            <label for="">อ.สัตหีบ จ.ชลบุรี 20250</label>
+                        </div>
+                        <div>
+                            <label for="">บ้านเลขที่</label>
+                            <label for="">______</label>
+                            <label for="">หมู่ที่</label>
+                            <label for="">______</label>
+                            <label for="">ซอย</label>
+                            <label for="">________________</label>
+                        </div>
+                        <div>
+                            <label for="">อำเภอ</label>
+                            <label for="">___________________</label>
+                            <label for="">จังหวัด</label>
+                            <label for="">_______________</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <div class="mt-3">
+                        <div style="margin-right: 4rem;">
+                            <label for="">วันที่</label>
+                            <label for="">______</label>
+                            <label for="">เดือน</label>
+                            <label for="">___________________</label>
+                            <label for="">พ.ศ.</label>
+                            <label for="">__________</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start mt-3">
+                    <div>
+                        <label for="">เรื่อง</label>
+                        <label for="" class="px-3">ขออนุญาต</label>
+                        <label for="" class=""><i class="bi bi-square"></i></label>
+                        <label for="" class="px-3">ลาป่วย</label>
+                        <label for="" class=""><i class="bi bi-square"></i></label>
+                        <label for="" class="px-3">ลากิจ</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start mt-1">
+
+                    <div class="mt-4">
+                        <label for="">เรียน</label>
+                        <label for="" class="px-3">เรียนผู้อำนวยการวิทยาลัยเทคนิคสัตหีบ</label>
+                    </div>
+                </div>
+                <div class="d-flex" style="margin-left: 11rem;">
+                    <div class="mt-3">
+                        <label for="">ด้วยข้าพเจ้า</label>
+                        <label for="" class="px-1">นาย/นางสาว</label>
+                        <label for="">_______________________________________________</label>
+                        <label for="" class="px-1">นักเรียน/นักศึกษา</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-1">
+                        <label for="">ระดับ</label>
+                        <label for="" class="px-1">___________</label>
+                        <label for="">กลุ่มที่</label>
+                        <label for="" class="px-1">___________</label>
+                        <label for="">แผนกวิชา</label>
+                        <label for="" class="px-1">________________________</label>
+                        <label for="">เลขประจำตัว</label>
+                        <label for="" class="px-1">_________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-1">
+                        <label for="">มีความประสงค์ขออนุญาต</label>
+                        <label for="" class="px-3"><i class="bi bi-square"></i></label>
+                        <label for="">ลาป่วย</label>
+                        <label for="" class="px-3"><i class="bi bi-square"></i></label>
+                        <label for="">ลากิจ</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-1">
+                        <label for="">เนื่องจาก</label>
+                        <label for="">__________________________________________________________________________________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex" style="margin-left: 11rem;">
+                    <div class="mt-3">
+                        <label for="">จึงขอเรียนมาเพื่อขออนุญาตลาเรียน ตั้งแต่เวลา</label>
+                        <label for="" class="px-1">______ น.</label>
+                        <label for="">วันที่</label>
+                        <label for="" class="px-1">______</label>
+                        <label for="">เดือน</label>
+                        <label for="" class="px-1">_______</label>
+                        <label for="">พ.ศ.</label>
+                        <label for="" class="px-1">______</label>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <div class="mt-3">
+                        <label for="">ถึงเวลา</label>
+                        <label for="" class="px-1">________ น.</label>
+                        <label for="">วันที่</label>
+                        <label for="" class="px-1">________</label>
+                        <label for="">เดือน</label>
+                        <label for="" class="px-1">_________________</label>
+                        <label for="">พ.ศ.</label>
+                        <label for="" class="px-1">___________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-4">
+                        <label for="">ขอแสดงความนับถือ</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-5">
+                        <label for="">ลงชื่อ</label>
+                        <label for="" class="px-1">___________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">(</label>
+                        <label for="" class="px-1">_______________________________</label>
+                        <label for="">)</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">นักเรียน นักศึกษา</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <label for="">ความเห็นของผู้ปกครอง</label>
+                        <label for="">_______________________________________________________________________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-3">
+                        <label for="">ขอแสดงความนับถือ</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-3">
+                        <label for="">ลงชื่อ</label>
+                        <label for="" class="px-1">___________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">(</label>
+                        <label for="" class="px-1">_______________________________</label>
+                        <label for="">)</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">ผู้ปกครอง</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <label for="">ความเห็นของครูที่ปรึกษา</label>
+                        <label for="">______________________________________________________________________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-3">
+                        <label for="">ขอแสดงความนับถือ</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-3">
+                        <label for="">ลงชื่อ</label>
+                        <label for="" class="px-1">___________________________</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">(</label>
+                        <label for="" class="px-1">_______________________________</label>
+                        <label for="">)</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="mt-2">
+                        <label for="">ครูที่ปรึกษา</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-1">
+                        <label for="">ครูประจำวิชาลงนามรับทราบ</label>
+                        <label for="" class="px-1 fs-5 " style="font-weight: bold;">(ให้ครูผู้สอนเซ็นชื่อเพื่อรับทราบด้วยทุกครั้งก่อนนำใบลาส่งงานครูที่ปรึกษา)</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <label for="">1.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">2.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">3.ชื่อวิชา</label>
+                        <label for="" class="">__________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <label for="">4.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">5.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">6.ชื่อวิชา</label>
+                        <label for="" class="">__________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <label for="">7.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">8.ชื่อวิชา</label>
+                        <label for="" class="">___________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                        <label for="">9.ชื่อวิชา</label>
+                        <label for="" class="">__________</label>
+                        <label for="">ลายเซ็นครู</label>
+                        <label for="" class="">_______</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-start">
+                    <div class="mt-3">
+                        <div>
+                            <label for="" style="font-weight: bolder;font-size: 1.1rem;text-decoration: underline;margin-right: 1rem;">หมายเหตุ</label>
+                            <label for="">1.นักศึกษานำ</label>
+                            <label for="" style="font-weight: bolder;text-decoration: underline;">ใบลากิจ</label>
+                            <label for="">ให้ครูที่ปรึกษาและครูผู้สอนลงนามรับทราบ ก่อนนำส่งงานครูที่ปรึกษา</label>
+                            <div style="margin-left: 6rem;">
+                                <label for="">2.ถ้านักศึกษา</label>
+                                <label for="" style="font-weight: bolder;text-decoration: underline;">ลาป่วยเกิน 3 วัน</label>
+                                <label for="">ต้องแนบใบรับรองแพทย์ และยื่นใบลาให้ครูผู้สอนลงนามรับทราบ</label>
+                            </div>
+                            <div style="margin-left: 6.7rem;">
+                                <label for="">ด้วยตัวเองทุกครั้ง</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</body>
+
+</html>
